cd59e4a79723421bad2193f4cf1d48f38351af09
subeen1007 <cc27c@naver.com>
2022-08-04 22:47:13 +0900

    section8.2 영화 검색 컴포넌트 테스트를 스프링 통합 테스트로 변경하기

--- src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -3,18 +3,37 @@ package moviebuddy.domain;
 import moviebuddy.MovieBuddyFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
 /**
  * @author springrunner.kr@gmail.com
  */
+@SpringJUnitConfig(MovieBuddyFactory.class)
+//@ExtendWith(SpringExtension.class)//JUnit이 테스트 실행 전략을 확장할 때 사용하는 애노테이션
+//@ContextConfiguration(classes = MovieBuddyFactory.class)
 public class MovieFinderTest {
 
-    final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
-    final MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
+//    필드에서 주입 but 권장X(테스트시 어려움 있음)
+    @Autowired MovieFinder movieFinder;
+
+//    생성자주입
+//    @Autowired
+//    MovieFinderTest(MovieFinder movieFinder){
+//        this.movieFinder = movieFinder;
+//    }
+
+//    setter에서 주입
+//    void setMovieFinder(MovieFinder movieFinder){
+//        this.movieFinder = movieFinder;
+//    }
 
     @Test
     void NotEmpty_directedBy() {
