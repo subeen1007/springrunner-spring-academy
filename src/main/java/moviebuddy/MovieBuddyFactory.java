def0e3a6445b16ddd7c63e940c3a1c57ae919b65
subeen1007 <cc27c@naver.com>
2022-08-04 22:06:37 +0900

    section6.3 자바 기반 빈 구성정보 조합하기

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -3,18 +3,26 @@ package moviebuddy;
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
 import moviebuddy.domain.MovieReader;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 @Configuration
+@Import({MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class})   //빈구성정보를 불러옴
 public class MovieBuddyFactory {
 
-    @Bean
-    public MovieReader movieReader(){
-        return new CsvMovieReader();//메소드 콜 방식
+    @Configuration
+    static class DomainModuleConfig{
+        @Bean
+        public MovieFinder movieFinder(MovieReader movieReader){
+            return new MovieFinder(movieReader);
+        }
     }
-    @Bean
-    public MovieFinder movieFinder(MovieReader movieReader){
-        return new MovieFinder(movieReader);//메소드 파라미터 방식
+
+    @Configuration
+    static class DataSourceModuleConfig{
+        @Bean
+        public MovieReader movieReader(){
+            return new CsvMovieReader();
+        }
     }
+
 }
