8e852b78c494f15a4ab9719c7f096adfa26d066e
subeen1007 <cc27c@naver.com>
2022-08-06 22:17:39 +0900

    section10.1 애플리케이션이 XML읽기 컴포넌트를 사용하도록 변경하기

--- code_changeset/section_07/src/main/java/moviebuddy/domain/MovieFinder.java
+++ code_changeset/section_07/src/main/java/moviebuddy/domain/MovieFinder.java
@@ -12,7 +12,7 @@ public class MovieFinder {
 	
 	private final MovieReader movieReader;
 
-	public MovieFinder(@Qualifier("csvMovieReader") MovieReader movieReader) {
+	public MovieFinder(@Qualifier("jaxbMovieReader") MovieReader movieReader) {
 		this.movieReader = Objects.requireNonNull(movieReader);
 	}
 	
